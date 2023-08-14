404b13ab5152924af0bf07fa61c42b046fdc9487
Mahdi Cherif <mehdichrif0@gmail.com>
2023-08-14 14:46:58 +0100

    added put and delete method and fixed link with the data base

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,21 @@
 const express = require('express');
+const mongoose = require('mongoose')
+
+const Thing = require('D:/STG/go-fullstack/backend/models/thing.js');
+
+mongoose.connect('mongodb+srv://MehdiC:MDXlpCno7ZGLMkHS@backend.drea6n1.mongodb.net/?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('sucess !'))
+  .catch(() => console.log('Fail !'));
+
 
 
 const app = express();
 
 app.use(express.json());
+
+
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -12,49 +24,36 @@ app.use((req, res, next) => {
   next();
 });
 
-app.post('/api/stuff',(req,res,next) =>{
-  console.log(req.body);
-  res.status(201).json({
-    message: 'Object created !'
+app.post('/api/stuff',(req,res,next) =>{ 
+  delete req.body._id;
+    const thing = new Thing({
+        ...req.body
+    });
+    thing.save()
+    .then(() => res.status(200).json({message: 'Object saved !'}))
+    .catch(error => res.status(400).json({ error }));
   });
-  next()
+
+app.put('/api/stuff/:id', (req, res, next) => {
+  Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Object Updated !'}))
+    .catch(error => res.status(400).json({ error }));
 });
 
+app.delete('/api/stuff/:id', (req, res, next) => {
+  Thing.deleteOne({ _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
+    .catch(error => res.status(400).json({ error }));
+});
 
-app.get('/api/stuff', (req, res, next) =>
- {
-    var stuff1 = [
-      {
-        _id: 'O3',
-        title: 'My first object',
-        description: 'About my first object',
-        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-        price: 4900,
-        userId: 'Admin',
-      }
-      
-    ]  
-    var stuff = [
-      {
-        _id: 'O1',
-        title: 'My first object',
-        description: 'About my first object',
-        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-        price: 4900,
-        userId: 'Admin',
-      },
-      {
-        _id: 'O2',
-        title: 'My second object',
-        description: 'About my second object',
-        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-        price: 2900,
-        userId: 'Admin1',
-      }
-    ];
-
-    stuff= stuff.concat(stuff1)
-    res.status(200).json(stuff);
+app.get('/api/stuff', (req, res, next) =>{
+    Thing.find()
+      .then(things => res.status(200).json(things))
+      .catch( error => res.status(400).json({ error }));
   });
-
+  app.get('/api/stuff/:id', (req, res, next) => {
+    Thing.findOne({ _id: req.params.id })
+      .then(thing => res.status(200).json(thing))
+      .catch(error => res.status(404).json({ error }));
+  });
 module.exports = app;
